75f8b2d4dcc1d15b3650c07227dbf1898c9b5577
arthurbraga-plank <arthur.braga@joinplank.com>
2023-04-28 13:35:55 -0300

    fix build

--- build/src/database/dataSource.js
+++ build/src/database/dataSource.js
@@ -6,6 +6,6 @@ const typeorm_1 = require("typeorm");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
     url: (_a = process.env.DB_URL) !== null && _a !== void 0 ? _a : "postgres://postgres@localhost:5432/rocket-launch",
-    entities: ["./src/models/*{.ts,.js}"],
-    migrations: ["./src/database/migrations/*{.ts,.js}"],
+    entities: ["./**/models/*.{ts,js}"],
+    migrations: ["./**/database/migrations/*.{ts,js}"],
 });
--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -3,6 +3,6 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DB_URL ?? "postgres://postgres@localhost:5432/rocket-launch",
-  entities: ["./src/models/*{.ts,.js}"],
-  migrations: ["./src/database/migrations/*{.ts,.js}"],
+  entities: ["./**/models/*.{ts,js}"],
+  migrations: ["./**/database/migrations/*.{ts,js}"],
 });
